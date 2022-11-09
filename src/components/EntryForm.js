2ed6efa1a300a40e5075ef2b9bb4ea588154bb55
Eduard Alexandru <eduard.github@gmail.com>
2022-11-09 05:03:22 +0000

    Completed EntryForm component, added new empty component

---
+++ src/components/EntryForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+// props.validateProduct( sku )     -- checks sku exists
+// props.addToBasket( {sku, qty} )  -- will do calcs and add it to basket list
+
+const EntryForm = ({ validateProduct, addToBasket }) => {
+	const [sku, setSku] = useState("");
+	const [qty, setQty] = useState(0);
+
+	const [invalidSKU, setInvalidSKU] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		let skuIsValid = validateProduct(sku);
+		setInvalidSKU(!skuIsValid);
+
+		if (skuIsValid) {
+			addToBasket({ sku, qty });
+			resetFields();
+		}
+	};
+
+	const resetFields = () => {
+		setSku("");
+		setQty(""); // 0?
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label for="sku">SKU: </label>
+			<input
+				type="text"
+				id="sku"
+				name="sku"
+				required
+				value={sku}
+				onChange={(e) => setSku(e.target.value)}
+			/>
+
+			<label for="qty">Quantity: </label>
+			<input
+				type="text"
+				id="qty"
+				name="qty"
+				required
+				value={qty}
+				onChange={(e) => setQty(e.target.value)}
+			/>
+
+			<button type="submit">Buy</button>
+			<button onClick={(e) => resetFields()}>Reset</button>
+
+			{invalidSKU && <p id="invalid-sku">The product doesn't exist!</p>}
+		</form>
+	);
+};
+
+export default EntryForm;
